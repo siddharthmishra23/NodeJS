bcd538b3560d060cb98f32a6df2c98a15c229727
siddharth.mishra2 <siddharth.mishra2@apple.com>
2020-09-14 09:57:53 +0530

    third commit

--- JSintro.js
+++ JSintro.js
@@ -636,6 +636,34 @@ r.toString()              // => "(1...3)"
 let Square = class { constructor(x) { this.area = x * x; } };
 new Square(3).area  // => 9
 
+// If the new String method startsWith() is not already defined...
+if (!String.prototype.startsWith) {
+    // ...then define it like this using the older indexOf() method.
+    String.prototype.startsWith = function(s) {
+        return this.indexOf(s) === 0;
+    };
+}
+
+// A trivial Array subclass that adds getters for the first and last elements.
+class EZArray extends Array {
+    get first() { return this[0]; }
+    get last() { return this[this.length-1]; }
+}
+
+let a = new EZArray();
+a instanceof EZArray  // => true: a is subclass instance
+a instanceof Array    // => true: a is also a superclass instance.
+a.push(1,2,3,4);      // a.length == 4; we can use inherited methods
+a.pop()               // => 4: another inherited method
+a.first               // => 1: first getter defined by subclass
+a.last                // => 3: last getter defined by subclass
+a[1]                  // => 2: regular array access syntax still works.
+Array.isArray(a)      // => true: subclass instance really is an array
+EZArray.isArray(a)    // => true: subclass inherits static methods, too!
+
+
+
+
 
 
 
