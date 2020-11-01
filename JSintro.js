eaa05d896bc12a0e4f8a654c317f67b07a1c8cae
siddharth.mishra2 <siddharth.mishra2@apple.com>
2020-11-01 15:05:29 +0530

    fourth

--- JSintro.js
+++ JSintro.js
@@ -663,6 +663,35 @@ EZArray.isArray(a)    // => true: subclass inherits static methods, too!
 
 
 
+A set is a collection of values, like an array is. Unlike arrays, however, sets are not ordered or indexed, and they do not allow duplicates: a value is either a member of a set or it is not a member; it is not possible to ask how many times a value appears in a set.
+
+let s = new Set();       // A new, empty set
+let t = new Set([1, s]); // A new set with two members
+The argument to the Set() constructor need not be an array: any iterable object (including other Set objects) is allowed:
+
+let s = new Set();  // Start empty
+s.size              // => 0
+s.add(1);           // Add a number
+s.size              // => 1; now the set has one member
+s.add(1);           // Add the same number again
+s.size              // => 1; the size does not change
+s.add(true);        // Add another value; note that it is fine to mix types
+s.size              // => 2
+s.add([1,2,3]);     // Add an array value
+s.size              // => 3; the array was added, not its elements
+s.delete(1)         // => true: successfully deleted element 1
+s.size              // => 2: the size is back down to 2
+s.delete("test")    // => false: "test" was not a member, deletion failed
+s.delete(true)      // => true: delete succeeded
+s.delete([1,2,3])   // => false: the array in the set is different
+s.size              // => 1: there is still that one array in the set
+s.clear();          // Remove everything from the set
+s.size              // => 0
+
+
+
+
+
 
 
 
